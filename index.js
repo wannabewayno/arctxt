0738e4921e09e9b911d3332027dce360d5fc827c
Wayne <wayne.c.griffiths@gmail.com>
2020-06-18 02:31:32 +0800

    updated distribution files to work with a class based system

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.onload = () => {
 
 const arctxt = () => {
     // this function will look for .arctxt classes and create a html collection
-    const arctxtCollection = document.getElementsByTagName('arctxt');
+    const arctxtCollection = document.getElementsByClassName('arctxt');
 
     // we create an array of arctxt elements from the html collection 
     const arctxtElements = Array.from(arctxtCollection);
